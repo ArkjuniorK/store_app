a3b233777e669ae243fffc0ddae1feeadae30bc0
ArkjuniorK <created.arkjuniork@gmail.com>
2021-06-27 18:21:39 +0700

    refactor: remove test route

--- api/cats.go
+++ api/cats.go
@@ -17,11 +17,6 @@
 package api
 
 import (
-	"fmt"
-	"math"
-	"net/http"
-	"strconv"
-
 	"github.com/go-chi/chi/v5"
 
 	"github.com/ArkjuniorK/store_app/controllers"
@@ -44,30 +39,4 @@ func Cats(r chi.Router) {
 	r.Delete("/{id}", Cat.DeleteCat)
 	r.With(middleware.SetImage).Post("/{id}", Cat.UploadImageCat)
 	r.Delete("/{id}/{id_image}", Cat.DeleteImageCat)
-	r.Get("/test/{page}/{limit}", func(w http.ResponseWriter, r *http.Request) {
-
-		// dummy data
-		nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19}
-		var slicedNums []int
-
-		// get params
-		// page, _ := strconv.ParseInt(chi.URLParam(r, "page"), 0, 8)
-		limit, _ := strconv.ParseFloat(chi.URLParam(r, "limit"), 0)
-
-		// total page
-		totalPage := float64(len(nums)) / limit
-
-		for i := 0; i < int(totalPage); i++ {
-			fmt.Println("index,", i)
-			// for j := 0; j < len(nums); j++ {
-			// 	if j < int(limit) {
-			// 		slicedNums = append(slicedNums, nums[j])
-			// 	}
-			// }
-		}
-
-		nums = nums[0:3]
-
-		fmt.Println(math.Ceil(totalPage), slicedNums, nums)
-	})
 }
